34af4b5c4877e898f16b44b6be13a746a30acef3
sal-tarek <sosototo427@gmail.com>
2024-08-21 00:07:14 +0300

    2 sections done

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const nav = document.querySelector('.navbar');
+const slide=document.querySelectorAll('.cover');
 let index=0;
-let cupList=[ `<iframe class="d-cupcake" src='https://my.spline.design/untitledcopy-9fed98a111675808c081397bcf2e95c1/' frameborder='0' width='100%' height='100%'></iframe>`,`<iframe class="d-cupcake" src='https://my.spline.design/untitled-cbd6be1503802087ade556a7a245b934/' frameborder='0' width='100%' height='100%'></iframe>`,`<iframe class="d-cupcake" src='https://my.spline.design/untitledcopycopy-0d2836aa7c36cf567c5b145e05f03e2a/' frameborder='0' width='100%' height='100%'></iframe>`,`<iframe  class="d-cupcake" src='https://my.spline.design/untitledcopycopycopy-729b008ab26d51c4cb9dccce2b29c66d/' frameborder='0' width='100%' height='100%'></iframe>`,`<iframe class="d-cupcake" src='https://my.spline.design/untitledcopycopycopycopy-2ef30cacaaae1d7c3ecf6acd93a6f68f/' frameborder='0' width='100%' height='100%'></iframe>`]
+let cupList=[ `<iframe class="d-cupcake" src='https://my.spline.design/untitledcopy-9fed98a111675808c081397bcf2e95c1/' frameborder='0' width='100%' height='100%'></iframe>`, `<iframe class="d-cupcake" src='https://my.spline.design/untitled-cbd6be1503802087ade556a7a245b934/' frameborder='0' width='100%' height='100%'></iframe>`,`<iframe  class="d-cupcake" src='https://my.spline.design/untitledcopycopycopy-729b008ab26d51c4cb9dccce2b29c66d/' frameborder='0' width='100%' height='100%'></iframe>`,`<iframe class="d-cupcake"  src='https://my.spline.design/spicedcinammon-ebcb8ce8b601a0b1bc6d990596506f4d/' frameborder='0' width='100%' height='100%'></iframe>`,`<iframe class="d-cupcake" src='https://my.spline.design/untitledcopycopycopycopy-2ef30cacaaae1d7c3ecf6acd93a6f68f/' frameborder='0' width='100%' height='100%'></iframe>`]
 menu.addEventListener('click', function(){
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
   nav.classList.toggle('is-active');
-})
+});
+slide.forEach(slide => {
+  slide.addEventListener('mouseover', () => {
+    slide.innerHTML = "Show More";
+  });
+
+  slide.addEventListener('mouseout', () => {
+    slide.innerHTML = ""; 
+  });
+});
 function change(direction){
   element=document.querySelector(".cupcakes");
   if(direction){
@@ -20,6 +30,7 @@ function change(direction){
   }
   element.innerHTML=cupList[index];
 }
+
 
 
 
